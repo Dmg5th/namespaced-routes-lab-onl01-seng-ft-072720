86227b84693eac588ac831fa30c3ee5f728bf92f
Dmg5th <Dmgould310@gmail.com>
2020-10-05 10:15:05 -0400

    Done.

--- db/migrate/20201005134430_create_preferences.rb
+++ db/migrate/20201005134430_create_preferences.rb
@@ -1,7 +1,8 @@
 class CreatePreferences < ActiveRecord::Migration[5.0]
   def change
     create_table :preferences do |t|
-      
+      t.boolean :create_new_song
+      t.boolean :create_new_artist
       t.timestamps
     end
   end
